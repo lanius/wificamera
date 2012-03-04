88afc5d8d60fc2464f1ed6783a2b19bfa7c35357
lanius <lanius@nirvake.org>
2012-03-04 17:49:33 +0900

    add a manifest file. fix a bug that fails to install by easy_install.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ for controlling the network camera CS-W07G-CY."""
 
 setup(
     name='wificamera',
-    version='0.1',
+    version='0.1.1',
     url='https://github.com/lanius/wificamera/',
     license='MIT',
     author='lanius',
